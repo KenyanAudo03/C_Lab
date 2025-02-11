1f6e09c761f1be84d68a360aaa4fd66a9dde2324
Victor Owino <vo4685336@gmail.com>
2025-02-11 12:39:52 +0300

    Add arithmetic operations, variable assignment, and basic output examples

---
+++ change_variable_values.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int myNum = 15;
+
+    int myOtherNum = 23;
+
+    myNum = myOtherNum;
+
+    printf("myNum is now: %d\n", myNum);
+
+    return 0;
+}
---
+++ variables.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int myNum = 15;          //  Integers
+    float myFloatNum = 5.99; // Floating point number
+    char myLetter = 'D';     // Character
+
+    printf("Hello, world! \n"); // String
+    printf("%d \n", myNum);
+    printf("%f\n", myFloatNum);
+    printf("%c\n", myLetter);
+    return 0;
+}
