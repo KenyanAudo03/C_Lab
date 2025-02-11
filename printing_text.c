40def2ebcb8d9fe27678a49beab8484960a4843a
Victor Owino <vo4685336@gmail.com>
2025-02-11 12:34:07 +0300

    Enhance printing_text.c with additional output and comments; add new variables binary file

--- printing_text.c
+++ printing_text.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-int main() {
-    printf("Hello, World!\n");
+int main()
+{
+    // New Line (\n)
+    printf("Hello, World!\n");  
+    printf("This is a simple C program.\n");
     return 0;
 }
+
+
+/* Multi-line comments*/
